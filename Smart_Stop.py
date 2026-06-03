10e7e28617e05e5645de121cdc4b894943752da4
NoScxhl16 <nolawimesfin6@gmail.com>
2026-06-02 23:42:40 -0400

    Build main screen and bus stop layout

--- Smart_Stop.py
+++ Smart_Stop.py
@@ -16,3 +16,14 @@ app.busCapacity=4
 app.totalStudentsAdded=0
 app.studentsPickedUp=0
 app.stepCounter=0
+
+# The drawing of the title and subtitle
+Label('Smart Stop',200,20,size=26,bold=True)
+Label('Bus Pickup Simulator',200,43,size=13)
+
+# The drawing for the bus stop area
+Rect(20,75,360,135,fill='lightGray',border='black')
+Rect(35,100,95,70,fill='white',border='black')
+Polygon(30,100,82,65,135,100,fill='darkSlateGray')
+Label('BUS STOP',82,128,size=11,bold=True)
+Label('Waiting Area',250,95,size=12,bold=True)
