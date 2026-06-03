324c0f476c0acd0e014280a3803a2a5d83b16d2c
NoScxhl16 <nolawimesfin6@gmail.com>
2026-06-02 23:45:27 -0400

    Add road and risk meter design

--- Smart_Stop.py
+++ Smart_Stop.py
@@ -27,3 +27,17 @@ Rect(35,100,95,70,fill='white',border='black')
 Polygon(30,100,82,65,135,100,fill='darkSlateGray')
 Label('BUS STOP',82,128,size=11,bold=True)
 Label('Waiting Area',250,95,size=12,bold=True)
+
+# Section for drawing the road and yellow lines
+Rect(0,220,400,55,fill='dimGray')
+Line(0,247,400,247,fill='yellow',lineWidth=3,dashes=True)
+Label('Pickup Road',200,290,size=12, bold=True)
+
+# Creates the risk meter that shows how unsafe the waiting conditions are.
+Rect(135,180,130,15,fill='white',border='black')
+riskBar=Rect(135,180,1,15,fill='green')
+Label('Risk Meter',200,173,size=10)
+
+
+
+
